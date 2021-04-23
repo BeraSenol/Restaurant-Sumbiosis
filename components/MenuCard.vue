571a68f0d4534c67c28c33deff2cf6f721748df9
Bera Senol <11700361@student.pxl.be>
2021-04-24 00:01:55 +0200

    Menu Carousel

---
+++ components/MenuCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <b-card id="card" :img-src="require(`~/static/images/${source}`)">
+    <b-card-title id="title"> {{ title }} </b-card-title>
+    <b-card-subtitle>
+      <h6 id="subtitle">{{ subtitle }}</h6>
+    </b-card-subtitle>
+    <b-card-text id="ingredients">
+      <p><span>Ingredienten: </span>{{ ingredients }}</p>
+    </b-card-text>
+    <b-card-text id="aromas">
+      <p><span>Aromas: </span>{{ aromas }}</p>
+    </b-card-text>
+    <template id="footer" #footer>
+      <p>
+        <span>Prijs:</span>
+      </p>
+      <p>
+        <span>&euro;{{ price }}</span>
+      </p>
+    </template>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: ["source", "title", "subtitle", "ingredients", "aromas", "price"],
+};
+</script>
+
+<style scoped>
+#card {
+  max-height: 450px;
+}
+
+#title {
+  padding-top: 0.5rem;
+  text-align: left;
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+  border-top: 0;
+}
+
+#subtitle {
+  text-align: left;
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+  font-weight: lighter;
+  font-style: italic;
+  font-size: 1rem;
+  padding-bottom: 1rem;
+  color: var(--gold);
+}
+
+#ingredients {
+  padding-bottom: 0.75rem;
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+  text-align: left;
+}
+
+#aromas {
+  padding-bottom: 0.75rem;
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+
+  text-align: left;
+}
+.card-footer {
+  border-top: 2px solid var(--gold);
+  display: flex;
+  justify-content: space-between;
+  padding: 0.75rem;
+}
+#img {
+  margin: 2rem;
+}
+
+.card-body {
+  padding: 0;
+}
+
+span {
+  font-style: italic;
+  font-weight: lighter;
+  color: var(--gold);
+}
+</style>
--- components/MenuCarousel.vue
+++ components/MenuCarousel.vue
@@ -1,21 +1,54 @@
 <template>
-  <b-carousel :interval="2500" class="dark-radial-overlay" controls indicators>
-    <b-carousel-slide img-src="~/static/images/menu-belgium.jpg">
-    </b-carousel-slide>
-    <b-carousel-slide img-src="~/static/images/menu-japan.jpg">
-    </b-carousel-slide>
-    <b-carousel-slide img-src="~/static/images/menu-italy.jpg">
-    </b-carousel-slide>
-    <b-carousel-slide img-src="~/static/images/menu-turkey.jpg">
-    </b-carousel-slide>
-    <b-carousel-slide img-src="~/static/images/menu-france.jpg">
-    </b-carousel-slide>
-  </b-carousel>
+  <div>
+    <b-carousel :interval="999999" controls indicators>
+      <b-carousel-slide img-src="~/static/images/menu-belgium.jpg">
+        <b-card-group deck>
+          <MenuCard
+            source="menu-belgium-stoofvlees.jpg"
+            title="Stoofvlees Met Frieten"
+            subtitle="Stoofvlees Met Frieten"
+            ingredients="Runderstoofvlees, Bruin Bier, Ajuin, Appelstroop, Rode Wijn Azijn, Boter"
+            aromas="Laurier, Tijm, Kruidnagel, Zout &amp; Peper"
+            price="17.99"
+          />
+          <MenuCard
+            source="menu-belgium-mussels.jpg"
+            title="Mosselen met frieten"
+            subtitle="Mosselen Met Frieten"
+            ingredients="Mosselen, Ajuin, Wortel, Knoflook, Rode Peper, Crème Fraîche"
+            aromas="Tijm, Peterselie, Zout &amp; Peper"
+            price="23.99"
+          />
+          <MenuCard
+            source="menu-belgium-chichon-au-gratin.jpg"
+            title="Witloof Gratin"
+            subtitle="Witloof Gratin"
+            ingredients="Witloof, Ham, Aardappelen, Gruyère, Parmezaan, Boter, Bloem"
+            aromas="Muskaatnoot, Peterselie zout &amp; peper"
+            price="13.99"
+          />
+        </b-card-group>
+      </b-carousel-slide>
+      <b-carousel-slide img-src="~/static/images/menu-japan.jpg">
+      </b-carousel-slide>
+      <b-carousel-slide img-src="~/static/images/menu-italy.jpg">
+      </b-carousel-slide>
+      <b-carousel-slide img-src="~/static/images/menu-turkey.jpg">
+      </b-carousel-slide>
+      <b-carousel-slide img-src="~/static/images/menu-france.jpg">
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
+.card-group {
+  display: flex;
+  flex-wrap: nowrap;
+  width: 100%;
+}
 </style>
